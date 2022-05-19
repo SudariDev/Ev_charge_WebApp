21d4715f71e0009a5ba86dbd4d69008ee7461414
Wasundara Samaranayaka <78250153+SudariDev@users.noreply.github.com>
2022-05-18 20:06:06 -0700

    Added StatCard.vue in components

---
+++ src/assets/components/CardView/StatCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card
+    class="mx-auto"
+    :color="color"
+    dark
+    style="height:120px;margin-top:12px"
+  >
+    <v-card-title>
+      <v-icon
+        large
+        left
+        style="font-size:50px"
+      >
+        {{icon}}
+      </v-icon>
+      <span class="text-h6 font-weight-light">{{title}}</span>
+    </v-card-title>
+
+    <v-card-text class="text-h5 font-weight-bold">
+       {{description}}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props:{
+        title:String,
+        icon :String,
+        color:String,
+        description :String
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
