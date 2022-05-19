0ea21af0bc11599c24e9c6fc3ec1a281c8cf7f41
Wasundara Samaranayaka <78250153+SudariDev@users.noreply.github.com>
2022-05-18 20:10:47 -0700

    Updated profile.vue file

--- src/View/Profile.vue
+++ src/View/Profile.vue
@@ -1,153 +1,211 @@
 <template>
-<div>
-    <b-container>  
-         <v-breadcrumbs :items="items"  divider=">" small class="Black" >
-           <template v-slot:item="{ item }">
-            <v-breadcrumbs-item v-if=item.disabled
-                :href="item.href"
-                :disabled="item.unable"
-            ><span style="color:black"> {{ item.text.toUpperCase() }}</span>
-               
-            </v-breadcrumbs-item>
-             <v-breadcrumbs-item v-if=item.active
-                :href="item.href"
-                :disabled="item.unable"
-            ><span style="color:blue"> {{ item.text.toUpperCase() }}</span>
-               
-            </v-breadcrumbs-item>
-            </template>
-         </v-breadcrumbs>
-    </b-container>
-   
-    
-<v-container>
-    
-        <v-row class="align" no-gutters>
-            
-
-            <v-col :align="align" no-gutters >
-                <v-card class="justify-xl-center cd1">
-                    <v-container class="justify-xl-center">
-                      <v-spacer></v-spacer>
-              
-                      <v-card flat class="rounded-0 mx-auto" max-width="100" :elevation="1">
-                            <v-img 
-                             class="#FFFFFF" v-bind:src="require('../assets/imges/user.png')" ></v-img>   
-                          </v-card>
-                    <v-card-text align="center"><h1 class="Black">Alison Burgas</h1></v-card-text>
-                    <v-card-text align="center"><h3 class="Black">967822406V</h3></v-card-text>
-                    <v-card-text align="center"><h3 class="">0717832129</h3></v-card-text>
-                    <v-card-text align="center"><h3 class="">Account Number: 84113556</h3></v-card-text>
-                    
-                 
-                    <v-spacer></v-spacer>
-                        <div class="green accent-10 text-center">
-                         <span class="white--text"> <h3>Balance Rs : {{ x }}</h3></span>
-                         </div>
-                         
-                
-                    </v-container>
-
-                </v-card>
-            </v-col>
-            <v-spacer></v-spacer>
-
-            <v-col no-gutters >
-        <v-card>      
-  <v-container>
-  
-    <form>
-      <v-text-field
-        v-model="name"
-        :error-messages="nameErrors"
-        :counter="10"
-        label="Name"
-        required
-        @input="$v.name.$touch()"
-        @blur="$v.name.$touch()"
-      ></v-text-field>
-      <v-text-field
-        v-model="email"
-        :error-messages="emailErrors"
-        label="E-mail"
-        required
-        @input="$v.email.$touch()"
-        @blur="$v.email.$touch()"
-      ></v-text-field>
-      <v-select
-        v-model="select"
-        :items="items"
-        :error-messages="selectErrors"
-        label="Item"
-        required
-        @change="$v.select.$touch()"
-        @blur="$v.select.$touch()"
-      ></v-select>
-      <v-checkbox
-        v-model="checkbox"
-        :error-messages="checkboxErrors"
-        label="Do you agree?"
-        required
-        @change="$v.checkbox.$touch()"
-        @blur="$v.checkbox.$touch()"
-      ></v-checkbox>
-
-      <v-btn
-        class="mr-4"
-        @click="submit"
+  <v-container
+    id="user-profile"
+    fluid
+    tag="section"
+  >
+    <v-row justify="center">
+      <v-col
+        cols="12"
+        md="8"
       >
-        submit
-      </v-btn>
-      <v-btn @click="clear">
-        clear
-      </v-btn>
-    </form>
-    
-  </v-container> 
-        </v-card>
-            </v-col>           
-       
-        </v-row>
         
-    
+<v-container>
+     <v-row class="align" no-gutters>
+         <v-col :align="align" no-gutters >
+            <v-card class="justify-xl-center cd1">
+              <v-form>
+            <v-container class="py-0">
+                    <v-row>
+                     <v-col
+                     cols="12"
+                     md="6"
+                    >
+                 
+                  <v-text-field
+                    class="purple-input"
+                    outlined
+                    dense
+                    label="First Name"
+                    :rules="username_rule"
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Last name"
+                    outlined
+                    dense
+                    class="purple-input"
+                    :rules="usernamelast_rule"
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    label="NIC"
+                    outlined
+                    dense
+                    class="purple-input"
+                    :rules="nic_rules"
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-text-field
+                    label="Email"
+                    outlined
+                    dense
+                    class="purple-input"
+                    :rules="email_rules"
+                  />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-text-field
+                    label="Address"
+                    outlined
+                    dense
+                    class="purple-input"
+                    :rules="phonenum_rules"
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    label="Mobile No"
+                    type="number"
+                    outlined
+                    dense
+                    class="purple-input"
+                    :rules="address_rule"
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  md="4"
+                >
+                  <v-text-field
+                    class="purple-input"
+                    outlined
+                    dense
+                    label="Password"
+                    type="password"
+                    :rules=" password_rule"
+                  />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-textarea
+                    class="purple-input"
+                    outlined
+                    dense
+                    label="About Me"
+                    :rules=" about_rule"
+                   
+                  />
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  class="text-right"
+                >
+                  <v-btn
+                    color="success"
+                    class="mr-0"
+                  >
+                    Update Profile
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+          </v-card>
+         </v-col>
+         
+     </v-row>
+</v-container>
+       
+      </v-col>
+
+
+      
+ 
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-card>  
+          <v-container>
+        <v-card flat class="rounded-0 mx-auto" max-width="100" :elevation="1">
+              <v-img class="#FFFFFF" v-bind:src="require('../assets/imges/user.png')" ></v-img>   
+                </v-card>
+          
+       <v-card-text align="center"><h4 class="display-2 font-weight-light mb-3 black--text">Alison Burgas</h4></v-card-text>
+       <v-card-text align="center"><h3 class="Black">967822406V</h3></v-card-text>
+       <v-card-text align="center"><h3 class="">0717832129</h3></v-card-text>
+       <v-card-text align="center"><h3 class="">Account Number: 84113556</h3></v-card-text>      
+        
+             <v-spacer></v-spacer>
+              <div class="green accent-10 text-center">
+              <span class="white--text"> <h3>Balance Rs : {{ x }}</h3></span>
+              </div>
+           
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
-   
-</div>
 </template>
 
 <script>
-
- export default {
-    data: () => ({
-      items: [
-        {
-          text: 'Home',
-          disabled: true,
-          href: 'breadcrumbs_dashboard',
-        },
-        {
-          text: 'Dashboard',
-          active: true,
-          href: 'breadcrumbs_link_1',
-        },
-        
+  export default {
+    data(){
+      return{
+        username_rule: [
+        (v) => !!v || "User Name is Required",
+        (v) => (v && v.length > 6) || "Invalid User name",
       ],
-       x:0,
+      usernamelast_rule: [
+        (v) => !!v || "User Name is Required",
+        (v) => (v && v.length > 6) || "Invalid User name",
+      ],
+       nic_rules: [
+        (v) => !!v || "NIC is Required",
+        (v) => (v && v.length < 11) || "Invalid NIC",
+      ],
+       email_rules: [
+        (v) => !!v || "Email is Required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+       phonenum_rules: [
+        (v) => !!v || "Phone Number is Required",
+        (v) => (v && v.length < 11) || "Invalid phone number",
+      ],
+      address_rule: [(v) => !!v || "Address is Required"],
+       password_rule: [
+        (v) => !!v || "Password is Required",
+        (v) => (v && v.length < 11) || "Invalid Password",
+      ],
+
+       about_rule: [(v) => !!v || "About is Required"]
 
 
-    }),
-
-
-    methods:{
-    
+      
+      }
     }
   }
-  
-
 </script>
-<style>
-.cd1{
-
-}
-
-</style>
