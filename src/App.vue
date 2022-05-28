804e7d98804d79e6dd05908e873d7c0ed846d354
Wasundara Samaranayaka <78250153+SudariDev@users.noreply.github.com>
2022-05-28 04:11:25 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,53 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      dark
-      class="#263238"
-    >
-        <div class="text-center mt-5">
-           <h1 class="white--text">ev.ceb.lk</h1>
-           <v-divider color="white" class="mt-4"></v-divider>
-            <v-avatar class="my-5" size="60">
-            <v-img src="https://cdn.vuetifyjs.com/images/lists/1.jpg" class="image"></v-img>
-            </v-avatar>
-            <h1 class="white--text">Alison Burgas</h1>
-            <h6 class="grey--text ">admin@admin.com</h6>     
-        </div>
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-          :to="item.path"
-        >
-          <v-list-item-icon>
-            <v-icon class="#FFFFFF">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title class="white--text">{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app elevation="1">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      
-      <v-spacer> </v-spacer>
-      
-    </v-app-bar>
-
-    <v-main class="blue-grey lighten-4">
-       
-      <router-view></router-view>
+<v-app  id="inspire" >
+  
+    <v-main>
+    
+    
+    <tool  v-if="validated" @drawerEvent="drawer = !drawer" />
+    <router-view/>
+    <SideBar v-if="validated" :drawer="drawer" />
+   
     </v-main>
   </v-app>
+ 
 </template>
 
 <script>
-  export default {
-    data: () => {
-      return {
-        drawer:false,
-        items:[
-          { title: 'Dashboard', icon: 'mdi-view-dashboard',path:"/Dashboard" },
-          { title: 'charge', icon: 'mdi-ev-plug-chademo',path:'/charge'},
-          { title: 'Profile', icon: 'mdi-account' ,path:"/Profile"},
-          { title: 'Map', icon: 'mdi-map-marker' ,path:"/Map"},
-          { title: 'Payment', icon: 'mdi-credit-card-outline' ,path:"/Payment"},
-          { title: "Reports", icon: "mdi-chart-areaspline",path:'/Report' },
-          { title: 'Notification', icon: 'mdi-bell' ,path:"/Notification"},
-          { title: 'Home',path:"/Home"},
-          
-        ]
-      }
 
+import SideBar from '../src/assets/components/SideBarSection/SideBarSection.vue'
+import tool from '../src/assets/components/ToolBar/TopBar.vue'
 
-  }
+export default{
+  name: "App",
+  components:{
+    tool,
+    SideBar,
+    
+  }, 
+ data() {
+    return {
+     drawer: null,
+    };
+  },
+  computed:{
+         validated(){
+           
+           if(this.$route.name == 'home'){
+              return  false;
+           }
+            else if(this.$route.name == 'login'){
+              return false;
+           }
+           else{
+             return true;
+           }
+           }
+         }
+        
 
+};
+
+ 
   
-  }
 </script>
