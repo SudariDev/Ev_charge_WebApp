1b68d3283e144b7353661bc15f7d2810dfe90a8c
Wasundara Samaranayaka <78250153+SudariDev@users.noreply.github.com>
2022-05-30 02:44:56 -0700

    Change button color

--- src/View/Home.vue
+++ src/View/Home.vue
@@ -8,10 +8,6 @@
         />
       </v-avatar>
 
-      <v-container>
-        <h3 style="color: #3DD807">EV Charge</h3>
-      </v-container>
-
       <v-tabs centered class="ml-n9" color="grey darken-1">
         <v-tab v-for="link in links" :key="link">
           {{ link }}
